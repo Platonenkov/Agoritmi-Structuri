499b90619fcf4f7d2fca239450d1603381167f9f
Evril <Platonenkov87@gmail.com>
2018-11-19 21:21:11 +0300

    урок 1

---
+++ ДЗ/1/algoritmy_lesson1/algoritmy_lesson1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace algoritmy_lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
