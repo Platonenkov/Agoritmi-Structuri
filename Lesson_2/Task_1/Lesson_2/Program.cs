f247cbaf400ec8e29655e104b374f3436880003c
Evril <Platonenkov87@gmail.com>
2018-11-25 18:47:16 +0300

    Первая задача

--- Lesson_2/Task_1/Lesson_2/Program.cs
+++ Lesson_2/Task_1/Lesson_2/Program.cs
@@ -6,18 +6,51 @@ namespace Lesson_2
     {
         static void Main(string[] args)
         {
-
-            int N = 2;
-            int temp = 0;
+            bool flag;
+            int N;
+            string temp = string.Empty;
             int system = 2;
 
-            while (N > 0)
+            while (true)
             {
-                temp = temp + N % system;
-                N = N / 10;
+                flag = false;
+                while (!flag)
+                {
+                    Console.WriteLine("Введите число для преобразования");
+                    flag = int.TryParse(Console.ReadLine(), out N);
+                    if (!flag) Console.WriteLine("Введено не число");
+                    else Console.WriteLine(Convert(N, system, temp));
 
+                }
             }
-            Console.Write(temp);
+        }
+
+        static string Convert(int N, int system, string temp)
+        {
+            if (N == 0) return ReversNumber(temp);
+            return temp=Convert(N/system, system, temp=temp+N%system).ToString();
+        }
+
+        static string ReversNumber(string stroka)
+        {
+            char[] s = stroka.ToCharArray();
+            if (s.Length < 4)
+            {
+                int temp = 4 - s.Length;
+                while (temp > 0)
+                {
+                    stroka = stroka + "0";
+                    temp--;
+                }
+            }
+            else stroka = string.Empty;
+
+            for (int i = s.Length-1; i >= 0; i--)
+            {
+                stroka = stroka + s[i];
+            }
+            return stroka;
         }
     }
+
 }
