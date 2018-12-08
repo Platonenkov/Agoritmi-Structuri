8af4d8fbf15cab0a0dde5d644bc03d46e911111d
Evril <Platonenkov87@gmail.com>
2018-12-08 15:51:47 +0300

    решена задача 3, 4

--- Lesson_5/Lesson5/Task_3/Program.cs
+++ Lesson_5/Lesson5/Task_3/Program.cs
@@ -10,17 +10,146 @@ namespace Task_2
         static Stack<char> bracket = new Stack<char>();
         static Stack<char> checkBraket = new Stack<char>();
 
+        static Ochered<char> openBracket = new Ochered<char>();
+        static Ochered<char> closingBracket = new Ochered<char>();
+
+        static char clbracket;
+
         static void Main(string[] args)
         {
-            stroka = "[2/{5*(4+7)}]";
-            Check(stroka);
+            stroka = "[2*{5*(4+7)}]";
+            int rez;
+            bool Proverka = Check(stroka);
+            if (Proverka == true)
+            {
+              rez = OperationProces(stroka);
+                Console.WriteLine($"Результат выражения {stroka} = {rez}");
+            }
+            else Console.WriteLine("Введены неверные данные, расставьте скобки правильно");
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Решение формулы
+        /// </summary>
+        /// <param name="stroka">формула в строчном представлении</param>
+        private static int OperationProces(string stroka)
+        {
+            Stack<char> formula = new Stack<char>();
+            var mass = stroka.ToCharArray();
+            int count = stroka.Length;
+            clbracket = closingBracket.Pop();
+
+            for (int i = mass.Length-1; i >=0; i--)
+            {
+                if (ClosingBracket(mass[i]))
+                {
+                    formula.Push(mass[i]);
+                    continue;
+                }
+                if (char.IsDigit(mass[i]))
+                {
+                    formula.Push(mass[i]);
+                    continue;
+                }
+                if (IsOperator(mass[i]))
+                {
+                    formula.Push(mass[i]);
+                    continue;
+                }
+                if (OpenBracket(mass[i],clbracket))
+                {
+                    ProcessOperation(formula);
+                    continue;
+                }
+            }
+            Stack<char> s = new Stack<char>();
+
+            while (formula.count > 0)
+            {
+               s.Push(formula.Pop());
+            }
+
+            string temp = string.Empty;
+            while (s.count > 0)
+            {
+                temp = temp + s.Pop();
+
+            }
+            return int.Parse(temp);
+
+        }
+
+
+        /// <summary>
+        /// решение части стека внутри скобок
+        /// </summary>
+        /// <param name="formula">стек символов</param>
+        private static void ProcessOperation(Stack<char> formula)
+        {
+            string a = string.Empty;
+            char simvol=' ';
+            string b = string.Empty; ;
+
+            var temp = formula.Pop();
+            while (!ClosingBracket(temp))
+            {
+                if (a == string.Empty)
+                    a = temp.ToString();
+                    while (true)
+                    {
+                        var temp1 = formula.Pop();
+                        if (char.IsDigit(temp1))
+                        {
+                            a = a + temp1;
+                        }
+                        else if (IsOperator(temp1)) { simvol = temp1; break; }
+                    }
+                if (b == string.Empty)
+                    while (true)
+                    {
+                        var temp1 = formula.Pop();
+                        if (char.IsDigit(temp1))
+                        {
+                            b = b + temp1;
+                        }
+                        else if (ClosingBracket(temp1)) { temp = temp1; clbracket = closingBracket.Pop(); break; }
+                    }
+            }
+
+                Operation(a, simvol, b, formula);
+            
+        }
+
+        /// <summary>
+        /// Выполнение операции над переменными
+        /// </summary>
+        /// <param name="a">левая переменная</param>
+        /// <param name="simvol">символ операции</param>
+        /// <param name="b">правая переменная</param>
+        /// <param name="formula">часть формулы на текущее выполнение</param>
+        private static void Operation(string a, char simvol, string b, Stack<char> formula)
+        {
+            char[] temp;
+            string s = string.Empty;
+            if (simvol == '*') s = (int.Parse(a) * int.Parse(b)).ToString();
+            else if (simvol == '/') s = (int.Parse(a) / int.Parse(b)).ToString();
+            else if (simvol == '-') s = (int.Parse(a) - int.Parse(b)).ToString();
+            else if (simvol == '+') s = (int.Parse(a) + int.Parse(b)).ToString();
+            temp = s.ToCharArray();
+            for (int i = 0; i < temp.Length; i++)
+            {
+                formula.Push(temp[i]);
+            }
+
         }
 
         /// <summary>
         /// проверка символов и распределение по стекам
         /// </summary>
         /// <param name="stroka"></param>
-        private static void Check(string stroka)
+        private static bool Check(string stroka)
         {
             var mass = stroka.ToCharArray();
             int count = mass.Length;
@@ -34,10 +163,14 @@ namespace Task_2
                 if (IsBracket(mass[i])) bracket.Push(mass[i]);
                 if (IsOperator(mass[i])) syvolOperator.Push(mass[i]);
             }
-            Console.WriteLine(CheckBracket(bracket));
+            return CheckBracket(bracket);
         }
 
-
+        /// <summary>
+        /// Проверка правильности расстановки скобок
+        /// </summary>
+        /// <param name="bracket">массив скобок</param>
+        /// <returns>истина или лож</returns>
         private static bool CheckBracket(Stack<char> bracket)
         {
 
@@ -52,13 +185,35 @@ namespace Task_2
             return true;
         }
 
+        /// <summary>
+        /// Проверяет явлется ли скобка открывающей
+        /// </summary>
+        /// <param name="v">символ</param>
+        /// <returns>истина или лож</returns>
         private static bool OpenBracket(char v)
         {
             var temp = (v == '(') ? ')' : (v == '{') ? '}' : ']';
             if (!(checkBraket.count == 0) && temp.Equals(checkBraket.Pop())) return true;
             return false;
         }
+        /// <summary>
+        /// Сверяет явлется ли скобка открывающей
+        /// </summary>
+        /// <param name="v">сверяемый символ</param>
+        /// <param name="clbracket">символ с которым сверяемся</param>
+        /// <returns>истина или лож</returns>
+        private static bool OpenBracket(char v, char clbracket)
+        {
+            var temp = (v == '(') ? ')' : (v == '{') ? '}' : ']';
+            if (temp.Equals(clbracket)) return true;
+            return false;
+        }
 
+        /// <summary>
+        /// Проверяет является ли скобка закрывающейся
+        /// </summary>
+        /// <param name="v">символ</param>
+        /// <returns>истина или лож</returns>
         private static bool ClosingBracket(char v)
         {
             if (v.Equals('}')) { checkBraket.Push(v); return true; }
@@ -92,12 +247,12 @@ namespace Task_2
         /// <returns>истина или ложь</returns>
         private static bool IsBracket(char v)
         {
-            if (v.Equals('{')) return true;
-            if (v.Equals('}')) return true;
-            if (v.Equals('(')) return true;
-            if (v.Equals(')')) return true;
-            if (v.Equals('[')) return true;
-            if (v.Equals(']')) return true;
+            if (v.Equals('{')) { openBracket.Push(v); return true; }
+            if (v.Equals('}')) { closingBracket.Push(v); return true; }
+            if (v.Equals('(')) { openBracket.Push(v); return true; }
+            if (v.Equals(')')) { closingBracket.Push(v); return true; }
+            if (v.Equals('[')) { openBracket.Push(v); return true; }
+            if (v.Equals(']')) { closingBracket.Push(v); return true; }
             return false;
         }
 
@@ -128,6 +283,37 @@ namespace Task_2
             {
                 --count;
                 return data[--position];
+
+            }
+
+
+        }
+
+        /// <summary>
+        /// Класс очередь
+        /// </summary>
+        /// <typeparam name="T">тип очереди</typeparam>
+        public class Ochered<T>
+        {
+            int head;
+            int tail;
+
+            T[] data = new T[100];
+            /// <summary>
+            /// Кладет данные в очередь
+            /// </summary>
+            /// <param name="obj">данные для внесения</param>
+            public void Push(T obj)
+            {
+                data[tail++] = obj;
+            }
+            /// <summary>
+            /// забирает данные из очереди
+            /// </summary>
+            /// <returns>данные</returns>
+            public T Pop()
+            {
+                return data[head++];
 
             }
 
---
+++ Lesson_5/Lesson5/Task_4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ochered<int> test = new Ochered<int>();
+            test.Push(10);
+            test.Push(5);
+            test.Push(2);
+            Console.WriteLine(test.Pop());
+            Console.WriteLine(test.Pop());
+            test.Push(6);
+            Console.WriteLine(test.Pop());
+
+        }
+    }
+    /// <summary>
+    /// Класс очередь
+    /// </summary>
+    /// <typeparam name="T">тип очереди</typeparam>
+    public class Ochered<T>
+    {
+        int head;
+        int tail;
+
+        T[] data = new T[100];
+        /// <summary>
+        /// Кладет данные в очередь
+        /// </summary>
+        /// <param name="obj">данные для внесения</param>
+        public void Push(T obj)
+        {
+            data[tail++] = obj;
+        }
+        /// <summary>
+        /// забирает данные из очереди
+        /// </summary>
+        /// <returns>данные</returns>
+        public T Pop()
+        {
+            return data[head++];
+
+        }
+
+
+    }
+}
