c09b163ed4beee2afa6dd2a4244f7dd5ff840f3d
Evril <Platonenkov87@gmail.com>
2018-12-15 16:01:18 +0300

    ДЗ 7 урок, обход графа методом Деикстра

---
+++ Lesson_7/Deyxtra/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deyxtra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Vershina
+    {
+        char name;
+        bool select;
+        bool check;
+
+    }
+}
